7246bb6269c729167a23f86fec7a42d361372859
Arkadiy Nazin <arcan.run@gmail.com>
2020-09-09 23:48:06 +0300

    task020: passed

--- src/main/java/com/epam/university/java/core/task020/Task020Impl.java
+++ src/main/java/com/epam/university/java/core/task020/Task020Impl.java
@@ -1,11 +1,25 @@
 package com.epam.university.java.core.task020;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Task020Impl implements Task020 {
     @Override
     public int calculate(Collection<String> stones) {
-        return 0;
+        if (stones == null || stones.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return stones.stream()
+                .map(e -> Arrays.stream(e.split("")).collect(Collectors.toSet()))
+                .reduce((a, b) -> {
+                    a.retainAll(b);
+                    return a;
+                }).get().size();
     }
 }
