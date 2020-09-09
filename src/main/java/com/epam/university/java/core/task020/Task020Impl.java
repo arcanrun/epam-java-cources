86d75b5f06d55d6f78b6f2b39fe0d3495a153603
Arkadiy Nazin <arcan.run@gmail.com>
2020-09-09 22:15:44 +0300

    init

---
+++ src/main/java/com/epam/university/java/core/task020/Task020Impl.java
@@ -0,0 +1,11 @@
+package com.epam.university.java.core.task020;
+
+
+import java.util.Collection;
+
+public class Task020Impl implements Task020 {
+    @Override
+    public int calculate(Collection<String> stones) {
+        return 0;
+    }
+}
