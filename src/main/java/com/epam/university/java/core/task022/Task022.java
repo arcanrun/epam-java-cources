5b8127a9981591d1da5c47ce7fcb0f04affa2441
Arkadiy Nazin <arcan.run@gmail.com>
2020-09-14 23:28:56 +0300

    task022: passed

--- src/main/java/com/epam/university/java/core/task022/Task022.java
+++ src/main/java/com/epam/university/java/core/task022/Task022.java
@@ -9,6 +9,7 @@ public interface Task022 {
     /**
      * Given collection of n integers, find the maximum value that can be calculated
      * by summing (n-1) integers.
+     *
      * @param numbers collection of numbers
      * @return maximum value
      */
@@ -17,6 +18,7 @@ public interface Task022 {
     /**
      * Given collection of n integer, find the minimum value that can ba calculated
      * by summing (n-1) integers.
+     *
      * @param numbers collection of numbers
      * @return minimum value
      */
--- src/main/java/com/epam/university/java/core/task022/Task022Impl.java
+++ src/main/java/com/epam/university/java/core/task022/Task022Impl.java
@@ -1,4 +1,25 @@
 package com.epam.university.java.core.task022;
 
-public class Task022Impl {
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Task022Impl implements Task022 {
+    @Override
+    public int maxSum(Collection<Integer> numbers) {
+        checkIsOk(numbers);
+        return numbers.stream().sorted().skip(1).mapToInt(e -> e).sum();
+    }
+
+    @Override
+    public int minSum(Collection<Integer> numbers) {
+        checkIsOk(numbers);
+        return numbers.stream().sorted().limit(numbers.size() - 1).mapToInt(e -> e).sum();
+    }
+
+    private void checkIsOk(Collection<Integer> collection) {
+        if (collection == null || collection.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
